bffdb3e763c0dc61ffe6cab72c9e48658d12b553
unknown <iokafor@FOR-WRKSTN-03.Formative.local>
2015-11-17 16:32:26 -0500

    more changes

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,1 +1,12 @@
-directives
+app.directive('ngEnter', function () {
+	return function(scope, element, attrs){
+		element.bind("keydown keypress", function (event){
+			if(event.which == 13){
+				scope.$apply(function(){
+					scope.$eval(attrs.ngEnter);
+				}
+				event.preventDefault();
+			}	
+		});
+	};
+});
