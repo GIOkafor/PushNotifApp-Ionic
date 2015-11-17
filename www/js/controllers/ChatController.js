1cab0614781ac54c2b84ddb77da208b4ef57a414
unknown <iokafor@FOR-WRKSTN-03.Formative.local>
2015-11-17 17:01:27 -0500

    add message in ChatCtrl done

--- www/js/controllers/ChatController.js
+++ www/js/controllers/ChatController.js
@@ -1,1 +1,22 @@
-ChatController
+app.controller('ChatController', function (socket) {
+	socket.on('connect', function(){
+		//add user called nickname
+		socket.emit('add user', 'nickname');
+	})
+
+	//function called when user hits send button
+	self.sendMessage=function(){
+		socket.emit('new message', self.message)
+		addMessageToList($stateParams.nickname, true, self.message)
+		socket.emit('stop typing');
+		self.message = "";
+	}
+
+	//add message
+	function addMessageToList (username, style_type, message) {
+		username = $sanitize(username);
+		var color = style_type ? getUsernameColor(username) : null;
+		self.messages.push({content: $sanitize(message), style:style_type, username:username, color:color});
+		$ionicScrollDelegate.scrollBottom();
+	}
+})
